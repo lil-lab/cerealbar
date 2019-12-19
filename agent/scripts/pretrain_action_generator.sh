4d6a82e3fa7bf173ac24625fd0e4772771a3c763
als495@cornell.edu <als495@cornell.edu>
2019-12-19 14:52:22 -0500

    Final versions of scripts for release.

--- agent/scripts/pretrain_action_generator.sh
+++ agent/scripts/pretrain_action_generator.sh
@@ -8,6 +8,7 @@ CUDA_VISIBLE_DEVICES=0 python -m agent.scripts.main \
                   --experiment_name=${EXPERIMENT_NAME} \
                   --maximum_generation_length=25 \
                   --model_type=ACTION_GENERATOR \
+                  --full_observability=True \
                   --use_goal_probabilities=True \
                   --use_trajectory_distribution=True \
                   --use_avoid_probabilities=True \
